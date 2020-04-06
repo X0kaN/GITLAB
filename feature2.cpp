30d99cd642b1de9b001251dcb5b8d0706cfc1d0c
Kirill2 <kirillersh2@gmail.com>
2020-04-06 18:59:06 +0300

    Sub and Del added

---
+++ feature2.cpp
@@ -0,0 +1,10 @@
+#include "feature2.h"
+#include <cmath>
+Double Sub(double a, double b)
+{
+return (a-b);
+}
+Double Del(double a, double b)
+{
+return (a/b);
+}
---
+++ feature2.h
@@ -0,0 +1,9 @@
+#ifndef __FEATURE2_H__
+#define __FEATURE2_H__  
+
+double Sub(double, double);
+
+double Del(double, double);
+
+#endif // __FEATURE2_H__ 
+
